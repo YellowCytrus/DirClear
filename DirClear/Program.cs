9e0b7f281ecb6f841a173119cd2c4ecb1cc5f29e
Cytrus <LancasterProxima@ya.ru>
2024-03-08 01:17:37 +0700

    Try to working with config.txt file

--- DirClear/Program.cs
+++ DirClear/Program.cs
@@ -1,14 +1,25 @@
-﻿namespace DirClear
+﻿using System.Text;
+
+namespace DirClear
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             string path = @"C:\\Users\\SadCy\\OneDrive\\Рабочий стол\\Новая папка";
+            string configPath = @"C:\\Codes\\C#\\Own\\DirClear\\DirClear\\config.txt";
 
-            
-
-            Router.Run(path);
+            using (FileStream fstream = File.OpenRead(configPath))
+            {
+                // выделяем массив для считывания данных из файла
+                byte[] buffer = new byte[fstream.Length];
+                // считываем данные
+                fstream.ReadAsync(buffer, 0, buffer.Length);
+                // декодируем байты в строкуs
+                string textFromFile = Encoding.Default.GetString(buffer);
+                Console.WriteLine($"Текст из файла: {textFromFile}");
+            }
+            //Router.Run(path);
         }
     }
 }
